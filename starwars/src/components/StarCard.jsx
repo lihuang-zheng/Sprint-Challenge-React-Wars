46d4b206959f0ba8d3726da165079e0cee7a16d3
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-24 12:06:13 -0600

    made the character name bigger to fit the visual needs. :)

--- starwars/src/components/StarCard.jsx
+++ starwars/src/components/StarCard.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Card, CardText, CardBody, CardTitle, Col } from "reactstrap";
 
+const titleStyle = {
+  fontSize: "2.5rem"
+};
+
 function StarCard(props) {
   return (
     <Col xs="12" md="6" xl="3">
       <Card>
         <CardBody>
-          <CardTitle>{props.name}</CardTitle>
+          <CardTitle style={titleStyle}>{props.name}</CardTitle>
           <CardText>Height: {props.height}</CardText>
           <CardText>Hair Color: {props.hairColor}</CardText>
           <CardText>Eye Color: {props.eyeColor}</CardText>
