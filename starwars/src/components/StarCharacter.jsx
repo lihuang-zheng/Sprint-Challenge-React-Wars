088c86f6890e672f51db0193b45d1b629e866a8e
lihuang-zheng <jerryzheng0131@gmail.com>
2020-01-24 11:30:19 -0600

    decided not to add photos from external due to bugs, and add boostrap and reacstrap.

--- starwars/src/components/StarCharacter.jsx
+++ starwars/src/components/StarCharacter.jsx
@@ -16,19 +16,9 @@ function StarCharacter() {
         console.log("the data was not return", error);
       });
   }, []);
+
   return (
     <div className="character">
-      <img src={StarWarsCharPic1} alt="Luke Skywalker" />
-      <img src={StarWarsCharPic2} alt="C3PO" />
-      <img src={StarWarsCharPic3} alt="R2D2" />
-      <img src={StarWarsCharPic1} alt="Darth Vader" />
-      <img src={StarWarsCharPic2} alt="Leia Organa" />
-      <img src={StarWarsCharPic3} alt="Owen Lars" />
-      <img src={StarWarsCharPic1} alt="Beru Whitesun lars" />
-      <img src={StarWarsCharPic3} alt="R5-D4" />
-      <img src={StarWarsCharPic2} alt="Biggs Darklighter" />
-      <img src={StarWarsCharPic3} alt="Obi-Wan Kenobi" />
-
       {character.map(character => {
         return <StarCard key={character.id} name={character.name} />;
       })}
